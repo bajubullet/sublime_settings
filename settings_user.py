a97e060e9646241e6eb0dca64092d57b49e32a31
Abhishek Bajpai <abajpai@akamai.com>
2015-12-03 13:04:20 +0530

    Added settings file.

---
+++ settings_user.py
@@ -0,0 +1,21 @@
+{
+    "color_scheme": "Packages/Theme - Nexus/Nexus.tmTheme",
+    "drag_text": false,
+    "ensure_newline_at_eof_on_save": true,
+    "find_selected_text": true,
+    "font_size": 14.0,
+    "highlight_line": true,
+    "ignored_packages":
+    [
+        "Vintage"
+    ],
+    "rulers":
+    [
+        80
+    ],
+    "scroll_past_end": true,
+    "show_full_path": true,
+    "theme": "Nexus.sublime-theme",
+    "translate_tabs_to_spaces": true,
+    "trim_trailing_white_space_on_save": true
+}
